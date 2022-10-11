afbe4fa3bbcd48b67c587c9a26d4f3dc172d9ae0
Joud Zeyadi <joud.zeyadi@kcl.ac.uk>
2022-10-11 18:59:00 +0100

    Solution 1

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+
+    if number_of_primes <= 0:
+        raise ValueError("Cannot be zero or negative")
+    
     list = []
+    count=2
+
+    while len(list) < number_of_primes:
+            t = True
+            for i in range(2, count):
+                if count % i == 0:
+                    t = False
+
+            if t:
+                list.append(count)
+            count+=1
     return list
